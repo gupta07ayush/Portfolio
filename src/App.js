b6d0da3bc0a914cbf832fa10c3fdd6c3dc51bd5e
Ayush Gutpa <gupta07ayush@gmail.com>
2025-09-23 00:03:11 +0530

    use HashRouter inplace of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
 import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Portfolio />} />
         </Routes>
         <Toaster />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
